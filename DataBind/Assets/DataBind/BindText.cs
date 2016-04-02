09ddc7224043369e53845b89f4871e66b3cad54a
paidgeek <tin.rabzelj@gmail.com>
2016-04-02 22:39:40 +0200

    Bind text update

---
+++ DataBind/Assets/DataBind/BindText.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+
+[RequireComponent(typeof(Text))]
+public class BindText : MonoBehaviour, IBindable
+{
+	private Text m_Text;
+	private string m_OriginalText;
+
+	private void Awake()
+	{
+		m_Text = GetComponent<Text>();
+		m_OriginalText = m_Text.text;
+	}
+
+	public void Bind(DataBindContext context)
+	{
+		m_Text.text = Regex.Replace(m_OriginalText, @"\{\{[^}]*}}", m => {
+			var key = m.Value.Substring(2, m.Value.Length - 4);
+
+			if (context.ContainsKey(key)) {
+				return context[key].ToString();
+			}
+
+			return m.Value;
+		});
+	}
+}
