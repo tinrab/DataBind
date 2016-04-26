0a7c5123bd0ad4adbbd9ebea4bfcc6d14853e3b2
paidgeek <tin.rabzelj@gmail.com>
2016-04-26 17:02:03 +0200

    bind property

---
+++ DataBind/Assets/DataBind/BindProperty.cs
@@ -0,0 +1,116 @@
+﻿using System.Reflection;
+using UnityEngine;
+
+public class BindProperty : MonoBehaviour
+{
+	public enum Direction
+	{
+		SourceUpdatesDestination,
+		DestinationUpdatesSource
+	}
+
+	public enum UpdateMethod
+	{
+		OnUpdate,
+		OnLateUpdate,
+		OnFixedUpdate
+	}
+
+	private PropertyInfo m_CachedDestinationProperty;
+	private PropertyInfo m_CachedSourceProperty;
+
+	[SerializeField]
+	private Component m_Destination;
+	[SerializeField]
+	private string m_DestinationProperty;
+	[SerializeField]
+	private Direction m_Direction;
+	[SerializeField]
+	private Component m_Source;
+	[SerializeField]
+	private string m_SourceProperty;
+	[SerializeField]
+	private UpdateMethod m_Update;
+	[SerializeField]
+	private bool m_UpdateInEditMode;
+
+	public Component source {
+		get { return m_Source; }
+		set { m_Source = value; }
+	}
+
+	public Component destination {
+		get { return m_Destination; }
+		set { m_Destination = value; }
+	}
+
+	public string sourceProperty {
+		get { return m_SourceProperty; }
+		set { m_SourceProperty = value; }
+	}
+
+	public string destinationProperty {
+		get { return m_DestinationProperty; }
+		set { m_DestinationProperty = value; }
+	}
+
+	public Direction direction {
+		get { return m_Direction; }
+	}
+
+	public UpdateMethod updateMethod {
+		get { return m_Update; }
+	}
+
+	public bool updateInEditMode {
+		get { return m_UpdateInEditMode; }
+	}
+
+	private void Update()
+	{
+		if (m_Update == UpdateMethod.OnUpdate) {
+			UpdateBind();
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		if (m_Update == UpdateMethod.OnFixedUpdate) {
+			UpdateBind();
+		}
+	}
+
+	private void LateUpdate()
+	{
+		if (m_Update == UpdateMethod.OnLateUpdate) {
+			UpdateBind();
+		}
+	}
+
+	public void UpdateBind()
+	{
+		if (m_SourceProperty == null || m_DestinationProperty == null) {
+			return;
+		}
+
+		if (m_CachedSourceProperty == null || m_CachedSourceProperty.Name != m_SourceProperty
+		    || m_CachedDestinationProperty == null || m_CachedDestinationProperty.Name != m_DestinationProperty) {
+			Cache();
+		}
+
+		switch (m_Direction) {
+			case Direction.SourceUpdatesDestination:
+				m_CachedDestinationProperty.SetValue(m_Destination, m_CachedSourceProperty.GetValue(m_Source, null), null);
+				break;
+			case Direction.DestinationUpdatesSource:
+				m_CachedSourceProperty.SetValue(m_Source, m_CachedDestinationProperty.GetValue(m_Destination, null), null);
+				break;
+		}
+	}
+
+	public void Cache()
+	{
+		m_CachedSourceProperty = m_Source.GetType().GetProperty(m_SourceProperty);
+		m_CachedDestinationProperty = m_Destination.GetType().GetProperty(m_DestinationProperty);
+	}
+}
---
+++ DataBind/Assets/DataBind/Editor/BindPropertyEditor.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof (BindProperty))]
+public class BindPropertyEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		var obj = (BindProperty) serializedObject.targetObject;
+		var source = serializedObject.FindProperty("m_Source");
+		var destination = serializedObject.FindProperty("m_Destination");
+
+		EditorGUILayout.PropertyField(source);
+
+		if (obj.source) {
+			var propertyDesc = ComponentsProperties(obj.source, obj.sourceProperty);
+
+			if (propertyDesc != null) {
+				obj.source = propertyDesc.component;
+				obj.sourceProperty = propertyDesc.name;
+			}
+		}
+
+		EditorGUILayout.PropertyField(destination);
+
+		if (obj.destination) {
+			var propertyDesc = ComponentsProperties(obj.destination, obj.destinationProperty);
+
+			if (propertyDesc != null) {
+				obj.destination = propertyDesc.component;
+				obj.destinationProperty = propertyDesc.name;
+			}
+		}
+
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("m_Direction"));
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("m_Update"));
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("m_UpdateInEditMode"));
+
+		if (obj.updateInEditMode) {
+			obj.UpdateBind();
+		}
+
+		serializedObject.ApplyModifiedProperties();
+	}
+
+	private PropertyDescription ComponentsProperties(Component component, string selectedProperty)
+	{
+		var components = component.GetComponents<Component>();
+		var propertyNames = new List<string>();
+		var properties = new List<PropertyDescription>();
+		var selectedIndex = -1;
+
+		foreach (var comp in components) {
+			foreach (var property in comp.GetType().GetProperties()) {
+				if (property.Name == selectedProperty) {
+					selectedIndex = propertyNames.Count;
+				}
+
+				propertyNames.Add(comp.name + "/" + property.Name);
+				properties.Add(new PropertyDescription(property.Name, comp));
+			}
+		}
+
+		var newSelected = EditorGUILayout.Popup("Property", selectedIndex, propertyNames.ToArray());
+
+		if (newSelected != selectedIndex) {
+			return properties[newSelected];
+		}
+
+		return null;
+	}
+
+	private class PropertyDescription
+	{
+		public PropertyDescription(string name, Component component)
+		{
+			this.name = name;
+			this.component = component;
+		}
+
+		public string name { get; set; }
+		public Component component { get; set; }
+	}
+}
